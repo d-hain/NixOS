141245b971d08d4f90c1cc5b8f9d2eecdd035a4d
David Hain <d.hain@gmx.at>
2025-08-03 17:33:02 +0200

    refactor

--- flake.nix
+++ flake.nix
@@ -30,34 +30,36 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = {
-        inherit hyprland ghostty fuzzel-pass;
+    nixosConfigurations = {
+      pc = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit hyprland ghostty fuzzel-pass;
+        };
+
+        modules = [
+          ./hosts/pc/configuration.nix
+        ];
       };
 
-      modules = [
-        ./hosts/pc/configuration.nix
-      ];
-    };
+      laptop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit hyprland ghostty fuzzel-pass;
+        };
 
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = {
-        inherit hyprland ghostty fuzzel-pass;
+        modules = [
+          ./hosts/laptop/configuration.nix
+        ];
       };
 
-      modules = [
-        ./hosts/laptop/configuration.nix
-      ];
-    };
+      server = server-nixpkgs.lib.nixosSystem {
+        inherit system;
 
-    nixosConfigurations.server = server-nixpkgs.lib.nixosSystem {
-      inherit system;
-
-      modules = [
-        ./hosts/server/configuration.nix
-      ];
+        modules = [
+          ./hosts/server/configuration.nix
+        ];
+      };
     };
 
     formatter.${system} = pkgs.alejandra;
