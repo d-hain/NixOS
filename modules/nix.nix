85cd9da2a5f25b39a72ceaeb4d003a30d85cd24f
David Hain <d.hain@gmx.at>
2025-11-30 18:09:55 +0100

    feat: enable auto optimise and gc

--- modules/nix.nix
+++ modules/nix.nix
@@ -3,4 +3,15 @@
     experimental-features = ["nix-command" "flakes"];
     auto-optimise-store = true;
   };
+
+  nix.optimise = {
+    automatic = true;
+    dates = ["weekly"];
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "monthly";
+    options = "--delete-older-than 30d";
+  };
 }
