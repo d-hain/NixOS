8ffe218838c93e82fc24ed6ce2e9b1f2b6a0c9ca
David Hain <d.hain@gmx.at>
2026-01-15 17:15:19 +0100

    feat: install obs-vkcapture plugin for game capture with veadotube

--- modules/programs.nix
+++ modules/programs.nix
@@ -17,10 +17,15 @@
     enable = true;
   };
 
-  # OBS as programs because of virtual camera
+  # OBS
   programs.obs-studio = {
     enable = true;
     enableVirtualCamera = true;
+
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-vaapi
+      obs-vkcapture
+    ];
   };
 
   ######################
