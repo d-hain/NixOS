a41524ca31b35e658bc0f3539784912bf0d77f79
David Hain <d.hain@gmx.at>
2026-01-22 22:25:20 +0100

    fix: shell: Home/End finally working again

--- modules/shell.nix
+++ modules/shell.nix
@@ -105,10 +105,8 @@
       bindkey "^[[1;3C" forward-word
       bindkey "^[[1;3D" backward-word
       # Home/End
-      bindkey '^[[H' beginning-of-line
-      bindkey '^[[F' end-of-line
-      bindkey '^[[1~' beginning-of-line
-      bindkey '^[[4~' end-of-line
+      bindkey '^[OH' beginning-of-line
+      bindkey '^[OF' end-of-line
       # Del Key actually delete char
       bindkey '\e[3~' delete-char
     '';
