796b5958c405f70614bcc683d923571b02d4ad93
David Hain <d.hain@gmx.at>
2026-02-21 14:42:12 +0100

    feat: shell: comments for syntax highlighting

--- modules/shell.nix
+++ modules/shell.nix
@@ -6,110 +6,118 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
 
-    promptInit = /* bash */ ''
-      autoload -U colors
-      colors
+    promptInit =
+      /*
+      bash
+      */
+      ''
+        autoload -U colors
+        colors
 
-      setopt PROMPT_SUBST
+        setopt PROMPT_SUBST
 
-      export CLICOLOR=1
-      export LSCOLORS=dxFxCxDxBxegedabagacad
-      VIRTUAL_ENV_DISABLE_PROMPT=1
+        export CLICOLOR=1
+        export LSCOLORS=dxFxCxDxBxegedabagacad
+        VIRTUAL_ENV_DISABLE_PROMPT=1
 
-      # Detect nix shell
-      in_nix_shell() {
-        if [[ -n "$IN_NIX_SHELL" ]]; then
-          echo -n " shell";
-        fi
-      }
-
-      # Detect venv
-      in_venv() {
-        if [[ -n "$VIRTUAL_ENV" ]]; then
-          echo -n " venv";
-        fi
-      }
-
-      # Detect shell level
-      shell_level() {
-        if [[ "$SHLVL" > 1 ]]; then
-          echo -n "%F{214}$SHLVL%f";
-        fi
-      }
-
-      #################
-      ### Git stuff ###
-      #################
-
-      git_branch() {
-        local branch
-        branch=$(git symbolic-ref --short HEAD 2>/dev/null) || return
-        echo -n " %F{yellow}on $branch%f";
-      }
-
-      git_dirty() {
-        if git -C "$dir" rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-          if [ -n "$(git -C "$dir" status --porcelain)" ]; then
-            echo -n " %F{red}✘%f";
-          else
-            echo -n " %F{green}✔%f";
+        # Detect nix shell
+        in_nix_shell() {
+          if [[ -n "$IN_NIX_SHELL" ]]; then
+            echo -n " shell";
           fi
-        fi
-      }
+        }
 
-      git_remote_status() {
-        local upstream ahead behind rev_list
+        # Detect venv
+        in_venv() {
+          if [[ -n "$VIRTUAL_ENV" ]]; then
+            echo -n " venv";
+          fi
+        }
 
-        upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null) || return
+        # Detect shell level
+        shell_level() {
+          if [[ "$SHLVL" > 1 ]]; then
+            echo -n "%F{214}$SHLVL%f";
+          fi
+        }
 
-        rev_list=$(git rev-list --left-right --count HEAD...@{u})
-        ahead=$(echo "$rev_list" | cut -f1)
-        behind=$(echo "$rev_list" | cut -f2)
+        #################
+        ### Git stuff ###
+        #################
 
-        echo -n "%F{yellow}($upstream"
+        git_branch() {
+          local branch
+          branch=$(git symbolic-ref --short HEAD 2>/dev/null) || return
+          echo -n " %F{yellow}on $branch%f";
+        }
 
-        ((  ahead > 0 )) && echo -n " %F{green}+$ahead%f"
-        (( behind > 0 )) && echo -n " %F{red}-$behind%f"
+        git_dirty() {
+          if git -C "$dir" rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+            if [ -n "$(git -C "$dir" status --porcelain)" ]; then
+              echo -n " %F{red}✘%f";
+            else
+              echo -n " %F{green}✔%f";
+            fi
+          fi
+        }
 
-        echo -n "%F{yellow})%f"
-      }
+        git_remote_status() {
+          local upstream ahead behind rev_list
 
-      status_line() {
-        local nix
-        nix=$(in_nix_shell)
-        [[ -n "$nix" ]] && echo -n " %F{blue}$nix%f";
-      }
+          upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null) || return
 
-      git_line() {
-        echo -n "$(git_branch)$(git_dirty) $(git_remote_status)"
-      }
+          rev_list=$(git rev-list --left-right --count HEAD...@{u})
+          ahead=$(echo "$rev_list" | cut -f1)
+          behind=$(echo "$rev_list" | cut -f2)
 
-      PROMPT=$'%F{green}╭─%n@%m%f$(status_line) %F{yellow}in %~%f$(git_line) $(shell_level)\n%F{green}╰$ %f'
-    '';
+          echo -n "%F{yellow}($upstream"
 
-    interactiveShellInit = /* bash */ ''
-      # Case-Insensitive Matching
-      zstyle ":completion:*" matcher-list "" "m:{a-zA-Z}={A-Za-z}"
+          ((  ahead > 0 )) && echo -n " %F{green}+$ahead%f"
+          (( behind > 0 )) && echo -n " %F{red}-$behind%f"
 
-      # Set zsh to Emacs mode so that Ctrl+R works
-      bindkey -e
+          echo -n "%F{yellow})%f"
+        }
 
-      # Keybinds for Navigation
+        status_line() {
+          local nix
+          nix=$(in_nix_shell)
+          [[ -n "$nix" ]] && echo -n " %F{blue}$nix%f";
+        }
 
-      # Ctrl+Arrows for word navigation
-      bindkey '^[[1;5C' forward-word
-      bindkey '^[[1;5D' backward-word
-      # Ctrl+Backspace delete word
-      bindkey '^H' backward-kill-word
-      # Alt+Arrows
-      bindkey "^[[1;3C" forward-word
-      bindkey "^[[1;3D" backward-word
-      # Home/End
-      bindkey '^[OH' beginning-of-line
-      bindkey '^[OF' end-of-line
-      # Del Key actually delete char
-      bindkey '\e[3~' delete-char
-    '';
+        git_line() {
+          echo -n "$(git_branch)$(git_dirty) $(git_remote_status)"
+        }
+
+        PROMPT=$'%F{green}╭─%n@%m%f$(status_line) %F{yellow}in %~%f$(git_line) $(shell_level)\n%F{green}╰$ %f'
+      '';
+
+    interactiveShellInit =
+      /*
+      bash
+      */
+      ''
+        # Case-Insensitive Matching
+        zstyle ":completion:*" matcher-list "" "m:{a-zA-Z}={A-Za-z}"
+
+        # Set zsh to Emacs mode so that Ctrl+R works
+        bindkey -e
+
+        # Keybinds for Navigation
+
+        # Ctrl+Arrows for word navigation
+        bindkey '^[[1;5C' forward-word
+        bindkey '^[[1;5D' backward-word
+        # Ctrl+Backspace delete word
+        bindkey '^H' backward-kill-word
+        # Alt+Arrows
+        bindkey "^[[1;3C" forward-word
+        bindkey "^[[1;3D" backward-word
+        # Home/End
+        bindkey '^[OH' beginning-of-line
+        bindkey '^[OF' end-of-line
+        # Del Key actually delete char
+        bindkey '\e[3~' delete-char
+      '';
 
     setOptions = [
       "AUTO_CD"
