f571e36b4904564a03e088fb8557962c64da899a
David Hain <d.hain@gmx.at>
2026-02-21 15:03:46 +0100

    Revert "fix: dotfiles: wireguard detection script"
    This reverts commit 06bbe19570db4d2674253d1c37e62797b9a78ab9.

--- dotfiles/.config/waybar/wireguard.sh
+++ dotfiles/.config/waybar/wireguard.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-if ip link show work >/dev/null 2>&1; then
-    echo '{ "text": "wg-work: UP", "class": "wireguard" }'
-else
-    echo ""
-fi
+while true
+do
+    systemctl is-active --quiet wg-quick-work.service
+    status=$?
+    if [[ status -eq 0 ]]; then
+        echo '{ "text": "wg-work: UP", "class": "wireguard" }'
+    else
+        echo ""
+    fi
+done
