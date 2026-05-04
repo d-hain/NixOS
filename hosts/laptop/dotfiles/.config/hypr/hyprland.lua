756ca3338352090d7dfe0a39ee568f9c56ea0bda
David Hain <d.hain@gmx.at>
2026-05-04 10:57:33 +0200

    feat: hypr: laptop: add fh beamer monitor mirror

--- hosts/laptop/dotfiles/.config/hypr/hyprland.lua
+++ hosts/laptop/dotfiles/.config/hypr/hyprland.lua
@@ -1,29 +1,41 @@
 -- Main Monitor
+-- hl.monitor {
+--   output = "HDMI-A-1",
+--   mode = "1920x1080@75",
+--   position = "0x0",
+--   scale = 1,
+--   bitdepth = 8,
+--   cm = "srgb",
+-- }
+
+-- TODO: Make toggle-able with HyprLua
+-- FH Beamer
 hl.monitor {
-    output = "HDMI-A-1",
-    mode = "1920x1080@75",
-    position = "0x0",
-    scale = 1,
-    bitdepth = 8,
-    cm = "srgb",
+  output = "HDMI-A-1",
+  mode = "1920x1080@60",
+  position = "0x0",
+  scale = 1,
+  mirror = "eDP-1",
+  bitdepth = 8,
+  cm = "srgb",
 }
 
 -- Second Monitor
 hl.monitor {
-    output = "eDP-1", -- Laptop inbuilt
-    mode = "1920x1080@60",
-    position = "1920x500",
-    scale = 1.2,
-    bitdepth = 8,
-    cm = "srgb",
+  output = "eDP-1", -- Laptop inbuilt
+  mode = "1920x1080@60",
+  position = "1920x500",
+  scale = 1.2,
+  bitdepth = 8,
+  cm = "srgb",
 }
 
 -- Other monitors
 hl.monitor {
-    output = "",
-    mode = "preferred",
-    position = "auto",
-    scale = "auto",
+  output = "",
+  mode = "preferred",
+  position = "auto",
+  scale = "auto",
 }
 
 hl.workspace_rule { workspace = "1", monitor = "HDMI-A-1" }
