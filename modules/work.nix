1bb6b06453b8a71cbdeb9d16932d908e0c2b360e
David Hain <d.hain@gmx.at>
2026-05-06 15:17:42 +0200

    fix: remove use of username in a string

--- modules/work.nix
+++ modules/work.nix
@@ -1,11 +1,15 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   user.packages = with pkgs; [
     kdePackages.krdc
   ];
 
   # Work Wireguard
   networking.wg-quick.interfaces.work = {
-    configFile = "/home/dhain/NAS-David/Work/WireGuard.conf";
+    configFile = "/home/${config.user.username}/NAS-David/Work/WireGuard.conf";
     autostart = false;
   };
 }
